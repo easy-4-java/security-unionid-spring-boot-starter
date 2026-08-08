ac17500eb1acfa2f5b7d1ecd2490799a75f874e2
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:26 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/test/java/org/springframework/security/boot/unionid/authentication/UnionIDAuthenticationProcessingFilterTest.java
+++ src/test/java/org/springframework/security/boot/unionid/authentication/UnionIDAuthenticationProcessingFilterTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link UnionIDAuthenticationProcessingFilter }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("UnionIDAuthenticationProcessingFilter Tests")
